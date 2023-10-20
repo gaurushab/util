4f961a662bd88cbc43ce3642fbe68392d63b7f2a
Gaurav Agrawal <gaurushab@gmail.com>
2023-10-19 22:22:34 -0500

    Update runrepo.sh

--- runrepo.sh
+++ runrepo.sh
@@ -28,7 +28,7 @@ git clone https://github.com/gaurushab/$REPONAME.git
 cd $REPONAME
 chmod +x *.sh
 echo "starting repo execution at "$(date)
-bash main.sh > ~/installation.log
+bash main.sh > installation.log
 echo "completed repo execution at "$(date)
 
 sudo umount -l /dev/sdb1
