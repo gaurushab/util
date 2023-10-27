3abe83b457e297fb3bd76912f08831013aa5761e
Gaurav Agrawal <gaurushab@gmail.com>
2023-10-27 15:22:14 -0500

    Update runrepo.sh

--- runrepo.sh
+++ runrepo.sh
@@ -5,6 +5,12 @@ export REPONAME=$1
 wget https://raw.githubusercontent.com/gaurushab/util/main/sysinit.sh
 bash sysinit.sh > sysinit.log
 
+sudo mkdir /secrets
+sudo mount /dev/sdb1 /secrets -o umask=0000
+
+sudo dnf install git -y
+git config --global credential.helper 'store --file /secrets/github'
+
 cd ~
 git clone https://github.com/gaurushab/$REPONAME.git
 cd $REPONAME
