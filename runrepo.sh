343274f8463b0d4f7a276adfbabf25f8df94b93e
Gaurav Agrawal <gaurushab@gmail.com>
2023-10-26 21:17:21 -0500

    Update runrepo.sh

--- runrepo.sh
+++ runrepo.sh
@@ -8,9 +8,12 @@ bash sysinit.sh > sysinit.log
 cd ~
 git clone https://github.com/gaurushab/$REPONAME.git
 cd $REPONAME
+export BASEDIR=$PWD
+echo 'export BASEDIR='$BASEDIR >> ~/.bashrc
 chmod +x *.sh
+
 echo "starting repo execution at "$(date)
-bash main.sh > installation.log
+bash main.sh > runrepo.log
 echo "completed repo execution at "$(date)
 
 # sudo umount -l /dev/sdb1
